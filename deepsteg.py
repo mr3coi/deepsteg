5175cab77f6ad00f27d4de8eec7b9fa29d711548
SJC <mr3coi@gmail.com>
2018-07-12 10:03:12 +0900

    Implemented skip connections in the hide network

--- deepsteg.py
+++ deepsteg.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 from functools import reduce
+from copy import deepcopy
 
 '''
 Notes:
@@ -11,7 +12,7 @@ Notes:
     - Output image is in 0-1 float format
 '''
 class DeepSteg(nn.Module):
-    def __init__(self, batch_size, im_dim, c="RGB", s="RGB"):
+    def __init__(self, batch_size, im_dim, c="RGB", s="RGB", skip=False):
         '''
         :param im_dim: the dimensions(height,width) of input image
         :type im_dim: tuple(int,int)
@@ -22,6 +23,7 @@ class DeepSteg(nn.Module):
         self._image_dim = im_dim
         self._batch_size = batch_size
         channels = {"RGB":3,"L":1}
+        self._h_skips = None
 
         # =========================== Prep network ===================================
         self._p_out_channel = 3     # assumed to be a multiple of '_p_kernel_num'
@@ -72,6 +74,11 @@ class DeepSteg(nn.Module):
         self._h_in_channels = [channels[c] + self._p_out_channel,] \
                                + [50 * self._h_kernel_num,] * (self._h_layer_num - 1)     # input dim
         self._h_out_channels = [50 * self._h_kernel_num,] * (self._h_layer_num - 1) + [channels[c],]     # final output dim
+        if skip:
+            self._h_skips = [(0,2),(2,4)]       # (layer_in, layer_out) (in terms of # input channels)
+            for skip_in, skip_out in self._h_skips:
+                self._h_in_channels[skip_out] += self._h_in_channels[skip_in]
+
         h_nets = [[nn.Conv2d(c_in, c_out // self._h_kernel_num, kernel_size=kernel, stride=1, padding=(kernel//2)) \
                         for c_in, c_out, kernel \
                         in zip(self._h_in_channels, self._h_out_channels, kernel_size)] \
@@ -156,9 +163,10 @@ class DeepSteg(nn.Module):
 
         # Run hiding network
         ### concatenate in the channels dimension
+        '''
         hidden_out = torch.cat((covers,prep_out), dim=1).to(device)    # hidden_in
 
-        for conv1, conv2, conv3, batchnorm, nls in zip(*self._h_net):
+        for it, (conv1, conv2, conv3, batchnorm, nls) in enumerate(zip(*self._h_net)):
             conv_res1 = conv1(hidden_out)
             conv_res2 = conv2(hidden_out)
             conv_res3 = conv3(hidden_out)
@@ -166,6 +174,32 @@ class DeepSteg(nn.Module):
             #hidden_out = reduce(torch.add, [conv_res1, conv_res2, conv_res3])
             hidden_out = batchnorm(hidden_out)
             hidden_out = nls(hidden_out)
+        '''
+        layer_inout = []
+        layer_inout.append(torch.cat((covers,prep_out), dim=1).to(device))    # hidden_in
+
+        if self._h_skips is not None:
+            do_skip = True
+            skip_iter = iter(self._h_skips)
+            skip_src, skip_dest = next(skip_iter, None)
+
+        for it, (conv1, conv2, conv3, batchnorm, nls) in enumerate(zip(*self._h_net)):
+            if do_skip and it == skip_dest:
+                new_hidden_in = torch.cat([layer_inout[-1], layer_inout[skip_src]], dim=1)
+                skip_items = next(skip_iter, None)
+                if skip_items is None:
+                    do_skip = False
+                else:
+                    skip_src, skip_dest = skip_items
+                layer_inout[-1] = new_hidden_in
+            conv_res1 = conv1(layer_inout[-1])
+            conv_res2 = conv2(layer_inout[-1])
+            conv_res3 = conv3(layer_inout[-1])
+            hidden_out = torch.cat([conv_res1, conv_res2, conv_res3], dim=1)
+            #hidden_out = reduce(torch.add, [conv_res1, conv_res2, conv_res3])
+            hidden_out = batchnorm(hidden_out)
+            hidden_out = nls(hidden_out)
+            layer_inout.append(hidden_out)
 
         # Run reveal network
         revealed_out = hidden_out.clone().to(device)
